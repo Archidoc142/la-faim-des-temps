e664b1bed7ee09cf2b032deddb244b62b707b658
itshichabk <itshichabk@gmail.com>
2024-09-21 22:44:28 -0400

    Components pour la page clients + autres pages admin à venir

--- resources/js/Pages/Admin/Table/Cell.jsx
+++
@@ -1,7 +0,0 @@
-export default function Cell({children}) {
-    return (
-        <td className="border even:bg-gray-100 text-center py-3">
-          {children}
-        </td>
-    )
-  }
--- resources/js/Pages/Admin/Table/HeadCell.jsx
+++
@@ -1,7 +0,0 @@
-export default function HeadCell({title}) {
-  return (
-    <th className="border py-3 w-20 bg-gray-300">
-        {title}
-    </th>
-  )
-}
