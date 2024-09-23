22abbe4240fb770435bac51ba8814a5bd9f8b766
itshichabk <itshichabk@gmail.com>
2024-09-23 11:36:53 -0400

    Début page accueil

--- resources/js/Pages/Accueil.jsx
+++ resources/js/Pages/Accueil.jsx
@@ -1,3 +1,4 @@
+import TitleSection from '@/Components/Accueil/TitleSection';
 import { Link, Head } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
 
@@ -9,15 +10,29 @@ export default function Accueil() {
         <>
             <Head title="Accueil"/>
             <div className=''>
-                <div>
+                <div className='bg-white'>
                     <div className='bg-white h-96 py-16'>
-                        <div className='w-80 m-auto'>
-                            <h1 className='font-serif text-[#04203f] text-center text-5xl'>Votre option traiteur et plats cuisinés au Centro</h1>
+                        <h1 className='w-80 m-auto font-serif text-[#04203f] text-center text-5xl'>Votre option traiteur et plats cuisinés au Centro</h1>
+                        <div className='flex justify-center'>
+                            <Link href="/menu">
+                                <button className='mt-7 uppercase bg-[#7A163C] text-white font-bold text-lg px-8 py-2 rounded-sm shadow-md hover:shadow-lg'>Menu de la semaine</button>
+                            </Link>
                         </div>
-                        <Link href="/menu">
-                            <button className='block m-auto mt-7 uppercase bg-[#7A163C] text-white font-bold text-lg px-8 py-2 rounded-sm '>Menu de la semaine</button>
-                        </Link>
+
                     </div>
+                    <div className='bg-[#04203f] py-12 px-12'>
+                        <h2 className='font-serif text-white text-center text-4xl font-medium mb-8 px-4'>
+                            Une cuisine de style bistro
+                        </h2>
+                        <p className='font-serif text-white text-center text-lg'>
+                            Ayant fait son apprentissage en cuisine française, mais sans jamais renier ses profondes inspirations américaines, notre chef crée des menus inspirés par les saisons et les produits locaux disponibles. Savourez nos mets préparés pour emporter en profitant de notre bistro pour le dîner. Une fois le travail fini, passez compléter votre épicerie avec notre gamme de produits entièrement québécoise afin de n'avoir à vous soucier que de savourer l'apéro.
+                        </p>
+                    </div>
+                    {/* image plat avec légende */}
+                    <TitleSection title="De la ferme..." color="bg-lime-600"/>
+                    {/* image ferme */}
+                    <TitleSection title="...à l'assiette!" color="bg-rose-900"/>
+                    {/* image plat avec légende */}
                 </div>
             </div>
         </>
