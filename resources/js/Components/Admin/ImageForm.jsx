c9720f38f6944da69d255dd28964129baa141f77
AmelieBergeron <ameber22@gmail.com>
2024-10-31 09:55:23 -0400

    CSS form admin image

--- resources/js/Components/Admin/ImageForm.jsx
+++ resources/js/Components/Admin/ImageForm.jsx
@@ -87,13 +87,13 @@ export default function ImageForm({ image, setPopupActif }) {
 
                         <div className="bg-white w-[850px] rounded-md p-6 z-40">
                             <div className="flex flex-nowrap justify-between align-middle w-full mb-10">
-                                <h2 className="text-3xl font-bold">{image ? "Modifier une image" : "Ajouter une nouvelle image"}</h2>
-                                <svg onClick={() => setPopupActif(false)} fill="black" height="160px" width="200px" className="h-fit self-center max-w-8 hover:fill-[#7A163C] cursor-pointer" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xmlSpace="preserve" stroke="none" >
+                                <h2 className="text-2xl font-bold">{image ? "Modifier une image" : "Ajouter une nouvelle image"}</h2>
+                                <svg onClick={() => setPopupActif(false)} fill="black" height="160px" width="200px" className="h-fit self-center max-w-7 hover:fill-[#7A163C] cursor-pointer" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 460.775 460.775" xmlSpace="preserve" stroke="none" >
                                     <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z" />
                                 </svg>
                             </div>
-                            <img className="h-52 mb-2 m-auto" src={imgSrc} alt={image ? image.alt : "image vide"} />
-                            <p className="italic text-sm text-center mb-6">{filename}</p>
+                            <img className="h-48 mb-2 m-auto" src={imgSrc} alt={image ? image.alt : "image vide"} />
+                            <p className="italic text-sm text-center mb-4">{filename}</p>
 
                             <div className="bg-[#7A163C] py-2 px-3 flex flex-nowrap w-fit rounded hover:bg-slate-700 cursor-pointer m-auto mb-6 relative">
                                 <svg className="self-center" width="18px" height="18px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
