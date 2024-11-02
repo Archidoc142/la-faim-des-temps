15cc18801aa91aa8f188b8ea9a50080b0f28c682
Hicham <itshichabk@gmail.com>
2024-11-02 21:57:10 +0000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin main
+git push
+
+# Install composer dependencies
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+npm run build
+
+# Run database migrations
+php artisan migrate:fresh --seed
+
+# Exit maintenance mode
+php artisan up
+
+echo "Deployment finished!"
