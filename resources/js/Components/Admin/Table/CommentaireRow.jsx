633922f3e8c86ce7db9d51f2701bf9c99aedc288
Archidoc142 <ardyn142@gmail.com>
2024-11-03 11:57:51 -0500

    Consulter Commentaire

--- resources/js/Components/Admin/Table/CommentaireRow.jsx
+++ resources/js/Components/Admin/Table/CommentaireRow.jsx
@@ -1,21 +1,33 @@
+import { useEffect } from "react";
 import ActionCell from "./ActionCell"
 import Cell from "./Cell"
 import StarsComment from "@/Components/StarsComment";
 
-export default function CommentaireRow({ commentaire, toggledMenuId, setToggledMenuId, page }) {
+export default function CommentaireRow({ commentaire, toggledMenuId, setToggledMenuId, page, setCommentaireShow, commentaireShow, setCommentaire }) {
 
     const options = [
-        { label: "Supprimer", route: "/admin/commentaire/destroy/" + commentaire.id, type: "DELETE" }
+        { label: "Supprimer", route: "/admin/commentaire/destroy/" + commentaire.id, type: "DELETE" },
     ]
 
     if (typeof commentaire.commentaire === "string") {
         options.unshift({ label: "Afficher/Cacher", route: "/admin/commentaire/toggle/" + commentaire.id, type: "PATCH" })
     }
 
-    return (
-        <tr className="h-10">
+    const toggleVisibility = () => {
+        setCommentaire(commentaire)
+        setCommentaireShow(true)
+    }
 
-            <Cell>{commentaire.commentaire ? commentaire.commentaire : "---"}</Cell>
+    return (
+        <tr className="h-10" >
+
+            <Cell className="cursor-pointer" onClick={() => toggleVisibility()}>
+                {commentaire.commentaire ?
+                    commentaire.commentaire.length >= 30 ?
+                        commentaire.commentaire.substring(0, 30) + "..."
+                        : commentaire.commentaire
+                    : "---"}
+            </Cell>
             <Cell>{commentaire.utilisateur}</Cell>
             <Cell className=""><StarsComment note={commentaire.note} updatable={false} className="justify-center" /></Cell>
             <Cell>{commentaire.masque ? "Oui" : "Non"}</Cell>
