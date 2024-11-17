e1a6f7108450cffadabdd8996ac6efbe4e58db0f
itshichabk <itshichabk@gmail.com>
2024-11-17 14:08:51 -0500

    Revert image seeders commits

--- database/seeders/ImageSaisonSeeder.php
+++ database/seeders/ImageSaisonSeeder.php
@@ -86,7 +86,16 @@ class ImageSaisonSeeder extends Seeder
             [
                 'id_image' => 4,
                 'id_saison' => 4,
-            ]
+            ],
+
+            [
+                'id_image' => 5,
+                'id_saison' => 2,
+            ],
+            [
+                'id_image' => 5,
+                'id_saison' => 3,
+            ],
         ]);
     }
 }
