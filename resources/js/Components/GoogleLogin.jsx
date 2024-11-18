6dc77b3785858570f3b17d9f0b44af0529307ef0
AmelieBergeron <ameber22@gmail.com>
2024-11-18 09:54:15 -0500

    fix css

--- resources/js/Components/GoogleLogin.jsx
+++ resources/js/Components/GoogleLogin.jsx
@@ -6,10 +6,10 @@ export default function GoogleLogin({ redirectToPanier }) {
 
     return (
         <a href={"/auth/redirect" + (redirectToPanier ? "?target=panier" : "")}>
-            <button type="button" className="w-full bg-blue-500 text-white my-4 px-4 py-2 font-bold rounded-md hover:shadow-md h-10">
+            <button type="button" className="w-full bg-blue-500 text-white my-4 px-4 py-2 rounded hover:shadow-md h-11">
                 <span className="flex gap-3 justify-center items-center">
                     <img src={googleLogo} alt="Google" height={24} width={24} />
-                    <p className="leading-4">{t("Login.google")}</p>
+                    <p className="leading-4 ">{t("Login.google")}</p>
                 </span>
             </button>
         </a>
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -30,7 +30,7 @@ export default function Login({ status, canResetPassword }) {
     const submit = (e) => {
         e.preventDefault();
 
-        if(!submitting) {
+        if (!submitting) {
             setSubmitting(true);
 
             post(route('login'), {
@@ -40,7 +40,7 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <div className='h-full md:p-8 xl:px-[8rem]'>
+        <div className='h-full md:p-12 xl:px-[8rem]'>
             <Head title="Log in" />
 
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
@@ -52,23 +52,23 @@ export default function Login({ status, canResetPassword }) {
                     visibility={messageVisibility}
                     setVisibility={setMessageVisibility}
                 />
-            : null}
-            <div className='bg-white rounded-lg m-6 flex min-h-[80%] 2xl:min-h-[90%] border-2 border-gray-500'>
-                <div className='hidden lg:block border-r-2 border-r-gray-500 w-[60%] p-6'>
+                : null}
+            <div className='bg-white rounded-lg flex md:max-w-[1200px] m-auto min-h-[80%] 2xl:min-h-[90%] border-2 border-gray-500'>
+                <div className='hidden lg:block border-r-2 border-r-gray-500 w-[55%] p-6'>
                     {/* div Image*/}
                     <div className='flex h-full'>
                         <img src={logo} alt="image-logo" className='block m-auto w-full' />
                     </div>
                 </div>
 
-                <div className='lg:w-[40%]'>
+                <div className='lg:w-[45%]'>
                     {/* div du formulaire (blanc)*/}
                     <div className='p-6 2xl:px-16'>
                         <h2 className='text-center font-bold text-2xl mb-8 mt-2'>{t("Login.title1")}<br />{t("Login.title2")}</h2>
 
-                        <GoogleLogin redirectToPanier={redirectToPanier}/>
+                        <GoogleLogin redirectToPanier={redirectToPanier} />
                         {/* div de la partie utile du formulaire (grise)*/}
-                        <div className='bg-[#f7f6f6] rounded-md p-6 2xl:pt-10 lg:px-10 lg:pb-10'>
+                        <div className='bg-[#EBEBEB] rounded-md mt-2 p-6 2xl:pt-10 lg:px-10 lg:pb-10'>
                             <form onSubmit={submit}>
 
                                 <div>
@@ -114,11 +114,16 @@ export default function Login({ status, canResetPassword }) {
                                             checked={data.remember}
                                             onChange={(e) => setData('remember', e.target.checked)}
                                         />
-                                        <span className="ms-2 text-sm text-gray-600">{t("Login.souvenir")}</span>
+                                        <span className="ms-2 text-sm text-gray-700">{t("Login.souvenir")}</span>
                                     </label>
                                 </div>
 
                                 <div className='text-right lg:mt-4 2xl:mt-12'>
+                                    <div className="flex justify-center mb-8 2xl:mt-14">
+                                        <button className={(submitting ? "bg-gray-400 hover:cursor-not-allowed" : "bg-[#0844a4] hover:bg-[#1f55ac]") + " text-white font-bold p-2 px-8 rounded-[4px]"}>
+                                            {t("Login.connexion")}
+                                        </button>
+                                    </div>
 
                                     {canResetPassword && (
                                         <Link
@@ -138,11 +143,7 @@ export default function Login({ status, canResetPassword }) {
                                         {t("Login.creer_compte")}
                                     </Link>
 
-                                    <div className="flex justify-center mt-8 2xl:mt-14">
-                                        <button className={(submitting ? "bg-gray-400 hover:cursor-not-allowed": "bg-[#0844a4] hover:bg-[#1f55ac]") + " text-white font-bold p-2 px-8 rounded-[4px]"}>
-                                            {t("Login.connexion")}
-                                        </button>
-                                    </div>
+
                                 </div>
                             </form>
                         </div>
