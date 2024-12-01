38f2c04f67f5c7c723b02702691414b798ae61b5
itshichabk <itshichabk@gmail.com>
2024-11-30 22:05:10 -0500

    Légendes images facultatives

--- app/Http/Resources/ImageAccueilResource.php
+++ app/Http/Resources/ImageAccueilResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\LegendeLangue;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -13,14 +14,17 @@ class ImageAccueilResource extends JsonResource
      * @return array<string, mixed>
      */
 
+
     public function toArray(Request $request): array
     {
+        $legendesExist = LegendeLangue::where("id_image", $this->id)->exists();
+
         return [
             "id" => $this->id,
             "src" => $this->nom_fichier,
             "legende" => [
-                "fr" => $this->langue("fr")->pivot->legende,
-                "en" => $this->langue("en")->pivot->legende,
+                "fr" => $legendesExist ? $this->langue("fr")->pivot->legende : "",
+                "en" => $legendesExist ? $this->langue("en")->pivot->legende : "",
             ]
         ];
     }
--- resources/js/Components/AccueilImgSaison.jsx
+++ resources/js/Components/AccueilImgSaison.jsx
@@ -4,9 +4,11 @@ export default function AccueilImgSaison({ classname, condition, src, alt, legen
             {condition ? src ?
                 <div className={"relative h-[500px] lg:h-[800px] " + classname}>
                     <img src={'../../../img/' + src} alt={alt} className='h-full w-full object-cover' />
-                    <div className="font-serif absolute bottom-0 min-h-12 flex items-center text-white bg-black bg-opacity-60 w-full px-4 lg:text-lg">
-                        {legend}
-                    </div>
+                    { legend ?
+                        <div className="font-serif absolute bottom-0 min-h-12 flex items-center text-white bg-black bg-opacity-60 w-full px-4 lg:text-lg">
+                            {legend}
+                        </div> :
+                    null }
                 </div>
             : null : null}
         </>
