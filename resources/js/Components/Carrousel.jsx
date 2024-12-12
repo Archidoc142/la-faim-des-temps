f983ea0bdafd7f1b489b87755aeb0f48d575cd4a
itshichabk <itshichabk@gmail.com>
2024-12-12 01:31:47 -0500

    z index

--- resources/js/Components/Carrousel.jsx
+++ resources/js/Components/Carrousel.jsx
@@ -22,7 +22,7 @@ export default function Carrousel({ images, i18n }) {
     return (
         <div className="bg-[#ba275b] pt-4 h-fit relative">
             <div className="flex justify-center items-center mx-[10%]">
-                <button className="absolute left-0 h-1/2 bg-[#ffffffb5] rounded-e-xl z-10" onClick={() => movePostLeft()}>
+                <button className="absolute left-0 h-1/2 bg-[#ffffffb5] rounded-e-xl" onClick={() => movePostLeft()}>
                     <svg className='min-w-12 md:max-w-24' viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2"><path d="M15 18l-6-6 6-6" /></svg>
                 </button>
 
@@ -31,11 +31,11 @@ export default function Carrousel({ images, i18n }) {
                         key={i}
                         src={`/img/${images[i].src}`}
                         alt={"img_plat_" + i}
-                        className={"max-h-[500px] xl:max-w-[60%] mb-6 mt-4 border-4 border-white " + (index == i ? "block" : "hidden")}
+                        className={"max-h-[500px] xl:max-w-[60%] mb-6 mt-4 border-4 border-white z-10 " + (index == i ? "block" : "hidden")}
                     />
                 )}
 
-                <button className="absolute right-0 h-1/2 bg-[#ffffffb5] rounded-s-xl z-10" onClick={() => movePostRight()}>
+                <button className="absolute right-0 h-1/2 bg-[#ffffffb5] rounded-s-xl" onClick={() => movePostRight()}>
                     <svg className='min-w-12 md:max-w-24' viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2"><path d="M9 18l6-6-6-6" /></svg>
                 </button>
             </div>
